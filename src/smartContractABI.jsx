d2820a83fac19a30d6817ba9c102b10449b0a170
Jay Parisi <jay.parisi@coinbase.com>
2022-12-09 19:10:56 -0500

    1.0 commit

--- src/smartContractABI.jsx
+++ src/smartContractABI.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import * as React from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { ethers } from 'ethers';
 
@@ -7,11 +7,20 @@ import {
   Box,
   Grid,
   VStack,
-  //   UnorderedList,
-  //   ListItem,
+  Input,
+  FormControl,
+  FormLabel,
 } from '@chakra-ui/react';
 
 export function FetchContract() {
+  const [currentPlayers, setCurrentPlayers] = React.useState([]);
+  const [currentManager, setCurrentManager] = React.useState('');
+  const [currentBalance, setCurrentBalance] = React.useState('');
+  const [currentValue, setCurrentValue] = React.useState('');
+  const [currentMessage, setCurrentMessage] = React.useState({
+    message: 'Waiting on transaction to process...',
+  });
+
   const { provider } = useWeb3React();
   const address = '0xF652E3004BF6025dFE423728257bC5096a0ACF99';
   const abi = [
@@ -80,13 +89,46 @@ export function FetchContract() {
       type: 'function',
     },
   ];
+  const signer = provider?.getSigner();
+  console.log(signer);
+  const callContract = new ethers.Contract(address, abi, signer);
   //const [error, setError] = useState(undefined);
-
   const connect = async () => {
-    const signer = provider?.getSigner();
-    const callContract = new ethers.Contract(address, abi, signer);
-    const players = await callContract.getPlayers();
-    console.log(players);
+    const manager = await callContract.manager();
+    setCurrentManager(manager);
+    const players = callContract.getPlayers();
+    setCurrentPlayers(players.length);
+    const balance = await provider.getBalance(callContract.address);
+    const etherBalance = ethers.utils.formatEther(balance);
+    console.log(etherBalance);
+
+    setCurrentBalance(etherBalance);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    console.log('clicked', currentValue, callContract);
+    console.log(currentValue * 10e17);
+    const weiPrice = currentValue * 10e17;
+
+    await callContract.enter({
+      value: '11000000000000000',
+    });
+
+    setCurrentMessage({ message: 'You have been entered!' });
+  };
+
+  const pickWinner = async () => {
+    //const accounts = await callContract.getPlayers();
+    setCurrentMessage({ message: 'Waiting on Transaction Success' });
+
+    await callContract.pickWinner({
+      gasLimit: 100000,
+    });
+
+    setCurrentMessage({
+      message: 'A winner has been picked',
+    });
   };
 
   return (
@@ -94,15 +136,29 @@ export function FetchContract() {
       <Grid minH="100vh" p={3}>
         <VStack spacing={8}>
           <Button onClick={connect}>Call Contract</Button>
-
-          {/* <UnorderedList>
-            <ListItem>isActive: {isActive.toString()}</ListItem>
-            <ListItem>isActivating: {isActivating.toString()}</ListItem>
-            <ListItem>accounts: {accounts}</ListItem>
-            <ListItem>account: {account}</ListItem>
-            <ListItem>chainId: {chainId}</ListItem>
-          </UnorderedList>
-          {isActive ? <Button onClick={disconnect}>Disconnect</Button> : ''} */}
+          <div>
+            <h2>Lottery Contract</h2>
+            <p>This contract is managed by {currentManager}.</p>
+            <p>
+              There are currently
+              {currentPlayers} entered and competing for {currentBalance}
+            </p>
+          </div>
+          <h4>Want to try your luck?</h4>
+          <FormControl>
+            <FormLabel>Amount of Ether to enter</FormLabel>
+            <Input
+              value={currentValue}
+              onChange={(event) => setCurrentValue(event.target.value)}
+            />
+            <Button onClick={onSubmit}>Enter</Button>
+          </FormControl>
+          <hr />
+          <h1>{currentMessage.message}</h1>
+          <hr />
+          <h4>Ready to Pick a winner?</h4>
+          <Button onClick={pickWinner}>Pick Winner</Button>
+          <h1>{currentMessage.message}</h1>
         </VStack>
       </Grid>
     </Box>
